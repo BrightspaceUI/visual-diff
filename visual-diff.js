baaf8fd7b594cf523335be85b97c764eefcaa597
Dave Batiste <dave.batiste@d2l.com>
2019-07-31 10:50:36 -0400

    Update getRect helper to account for possible offsetLeft or offsetTop that is less than the specified margin.

--- visual-diff.js
+++ visual-diff.js
@@ -83,11 +83,13 @@ class VisualDiff {
 	async getRect(page, selector, margin) {
 		margin = (margin !== undefined) ? margin : 10;
 		return page.$eval(selector, (elem, margin) => {
+			const leftMargin = (elem.offsetLeft < margin ? 0 : margin);
+			const topMargin = (elem.offsetTop < margin ? 0 : margin);
 			return {
-				x: elem.offsetLeft - margin,
-				y: elem.offsetTop - margin,
-				width: elem.offsetWidth + (margin * 2),
-				height: elem.offsetHeight + (margin * 2)
+				x: elem.offsetLeft - leftMargin,
+				y: elem.offsetTop - topMargin,
+				width: elem.offsetWidth + (leftMargin * 2),
+				height: elem.offsetHeight + (topMargin * 2)
 			};
 		}, margin);
 	}

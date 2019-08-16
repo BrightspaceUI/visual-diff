1e282f7578f148280fa0932c26af9e7a7f1a4508
Margaree Peacocke <margaree.p@gmail.com>
2019-08-16 10:58:51 -0400

    let instead of const

--- visual-diff.js
+++ visual-diff.js
@@ -6,7 +6,7 @@ const polyserve = require('polyserve');
 const FileHelper = require('./file-helper.js');
 
 const _isGoldenUpdate = process.argv.includes('--golden') ? process.argv.includes('--golden') : false;
-const _isCI = process.env['CI'] ? true : false;
+let _isCI = process.env['CI'] ? true : false;
 _isCI = false;
 const _serverOptions = {npm: true, moduleResolution: 'node'};
 

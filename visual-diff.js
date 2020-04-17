37665b6c6beda1bcd5dfc03cd313bed45633b3ce
Dave Batiste <dave.batiste@d2l.com>
2020-04-17 13:49:52 -0400

    Set default viewport size as part of createPage helper.

--- visual-diff.js
+++ visual-diff.js
@@ -76,11 +76,16 @@ class VisualDiff {
 
 	}
 
-	async createPage(browser) {
+	async createPage(browser, options) {
 		const page = await browser.newPage();
 		await page.emulateMediaFeatures([{
 			name: 'prefers-reduced-motion', value: 'reduce'
 		}]);
+		const viewportOptions = {width: 800, height: 800, deviceScaleFactor: 2};
+		if (options && options.viewport) {
+			Object.assign(viewportOptions, options.viewport);
+		}
+		await page.setViewport(viewportOptions);
 		return page;
 	}
 
